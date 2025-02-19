8e810d6c9b4392359b0665ee221984082a2a132b
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-02-19 15:55:15 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,34 +2,12 @@
 
 set -ex
 
-echo "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-echo "uname -m"
-uname -m
-echo "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-
-echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-echo "PREFIX: ${PREFIX}"
-echo "Python: ${PYTHON}"
-echo "Which Python: "
-which python
-echo "Version: "
-python --version
-echo "Which Python3"
-which python3
-echo "Python3 Version: "
-python3 --version
-echo "Python ls: "
-ls $PREFIX/bin/python
-echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-
 # Configure step
 cmake -Bbuild -GNinja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DPython_EXECUTABLE=${PYTHON} \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPython_EXECUTABLE=$PREFIX/bin/python3 \
     -DBUILD_SHARED_LIBS=ON \
     -DMDI_Fortran=ON \
     -DMDI_Python=ON \
