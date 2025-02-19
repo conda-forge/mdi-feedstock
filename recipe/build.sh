08d199e33787c17864c91517218b47136b61e099
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-02-19 09:55:01 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,22 @@
 
 set -ex
 
+export PATH=$BUILD_PREFIX/bin:$PATH  # Ensure conda python is used
+
+echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+echo "Python: ${PYTHON}"
+echo "Version: "
+python --version
+echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
 # Configure step
 cmake -Bbuild -GNinja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DPython_EXECUTABLE=${PYTHON} \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DPython_EXECUTABLE=$PREFIX/bin/python3 \
     -DBUILD_SHARED_LIBS=ON \
     -DMDI_Fortran=ON \
     -DMDI_Python=ON \
